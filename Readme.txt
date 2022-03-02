935a0ac4a5e07dca3a3eb4ab5b973cad245b8f93
AmjadIsmail <66602030+AmjadIsmail@users.noreply.github.com>
2022-03-02 10:11:58 +0000

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+﻿
+Extensions/ManageExtensions/Specflow for visual studio
+
+Adding
+Specflow
+nunit test adaptor
+SpecFlow.nunit
+Specflow.tools.msbuild
+Specflow.assist.dynamic
---
+++ Steps/LoginSteps.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecFlowNetCoreDemo.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowNetCoreDemo.Steps
+{
+    [Binding]
+    public sealed class LoginSteps
+    {
+
+        // Anti-context Injection Code -- 100% wrong
+        LoginPage loginpage = null;
+
+        [Given(@"I launch the application")]
+        public void GivenILaunchTheApplication()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            loginpage = new LoginPage(webDriver);
+
+        }
+
+        [Given(@"I click login link")]
+        public void GivenIClickLoginLink()
+        {
+            loginpage.ClickLogin();
+        }
+
+        [Given(@"I enter the following details")]
+        public void GivenIEnterTheFollowingDetails(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            loginpage.Login((string)data.UserName, (string)data.Password);
+
+        }
+
+        [Given(@"I click login button")]
+        public void GivenIClickLoginButton()
+        {
+            loginpage.ClickLoginButton();
+
+        }
+
+        [Then(@"I should see Employee details link")]
+        public void ThenIShouldSeeEmployeeDetailsLink()
+        {
+            Assert.That(loginpage.IsEmployeeDetailsExists(), Is.True);
+        }
+
+    }
+}
